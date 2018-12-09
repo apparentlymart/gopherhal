23432935dd1eb4960eb856d1bec179c2ee1f9241
Martin Atkins <mart@degeneration.co.uk>
2018-12-09 04:47:48 +0000

    Helper methods for working with sentences and words

--- ghal/sentence.go
+++ ghal/sentence.go
@@ -13,6 +13,8 @@ type Word struct {
 	Text string
 }
 
+var Period = MakeWord(".", ".")
+
 func MakeWord(tag, text string) Word {
 	text = strings.ToLower(norm.NFC.String(text))
 	return Word{tag, text}
@@ -22,7 +24,79 @@ func (w Word) GoString() string {
 	return fmt.Sprintf("ghal.MakeWord(%q, %q)", w.Tag, w.Text)
 }
 
+func (w Word) IsNoun() bool {
+	switch w.Tag {
+	case "NN", "NNS", "NNP", "NNPS":
+		return true
+	default:
+		return false
+	}
+}
+
+func (w Word) IsProperNoun() bool {
+	switch w.Tag {
+	case "NNP", "NNPS":
+		return true
+	default:
+		return false
+	}
+}
+
 type Sentence []Word
+
+// Words returns a set of all of the distinct words in the sentence.
+func (s Sentence) Words() WordSet {
+	ret := make(WordSet, len(s))
+	for _, w := range s {
+		ret.Add(w)
+	}
+	return ret
+}
+
+// Nouns returns a set of all of the distinct nouns in the sentence.
+func (s Sentence) Nouns() WordSet {
+	ret := make(WordSet, len(s))
+	for _, w := range s {
+		if w.IsNoun() {
+			ret.Add(w)
+		}
+	}
+	return ret
+}
+
+// ProperNouns returns a set of all of the distinct proper nouns in the sentence.
+func (s Sentence) ProperNouns() WordSet {
+	ret := make(WordSet, len(s))
+	for _, w := range s {
+		if w.IsProperNoun() {
+			ret.Add(w)
+		}
+	}
+	return ret
+}
+
+// TrimPeriod tests whether the final "word" in the receiver is a period and
+// if so returns a new slice with the same backing array that does not include
+// that trailing period. Otherwise, returns the receiver verbatim.
+//
+// This method does not trim any other sort of sentence-terminating punctuation,
+// such as question marks. It is intended to emulate common casual chatroom
+// writing style where periods are usually elided at the ends of sentences.
+//
+// This can either be used prior to adding a new sentence to a brain (to
+// cause the brain to learn sentences without trailing periods) or on a
+// sentence constructed by a brain (to cosmetically remove trailing periods
+// even though the brain itself considers them part of a sentence).
+func (s Sentence) TrimPeriod() Sentence {
+	switch {
+	case len(s) == 0:
+		return s
+	case s[len(s)-1] == Period:
+		return s[:len(s)-1]
+	default:
+		return s
+	}
+}
 
 func (s Sentence) String() string {
 	var ret strings.Builder
@@ -54,6 +128,28 @@ func (s WordSet) Has(k Word) bool {
 
 func (s WordSet) Add(k Word) {
 	s[k] = struct{}{}
+}
+
+// Nouns returns the set of words within the receiver that are nouns.
+func (s WordSet) Nouns() WordSet {
+	ret := make(WordSet)
+	for w := range s {
+		if w.IsNoun() {
+			ret.Add(w)
+		}
+	}
+	return ret
+}
+
+// ProperNouns returns the set of words within the receiver that are proper nouns.
+func (s WordSet) ProperNouns() WordSet {
+	ret := make(WordSet)
+	for w := range s {
+		if w.IsProperNoun() {
+			ret.Add(w)
+		}
+	}
+	return ret
 }
 
 // ChooseRandom will choose up to n words pseudo-randomly from the receiving
